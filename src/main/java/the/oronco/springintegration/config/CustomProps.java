75b813acfaeed7f112ecd5afa3f9298c7a34fae5
Theo Roncoletta <theo.roncoletta@atr-software.de>
2024-01-31 12:56:48 +0100

    added a map to my custom props

--- src/main/java/the/oronco/springintegration/config/CustomProps.java
+++ src/main/java/the/oronco/springintegration/config/CustomProps.java
@@ -3,16 +3,21 @@ package the.oronco.springintegration.config;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
+
+import java.util.Map;
 
 @ConfigurationProperties("the.oronco")
 @Validated
 public record CustomProps(@DefaultValue("false") boolean yes,
                           @NotEmpty @DefaultValue("nothing") String whatIsThis,
                           @Valid @DefaultValue Nested nested,
-                          @NotEmpty @DefaultValue("pong") String pongMessage) {
+                          @NotEmpty @DefaultValue("pong") String pongMessage,
+                          Map<@NotEmpty String, @NotNull Boolean> mappedy) {
 
     private enum BirbMode {
         FLYING,
